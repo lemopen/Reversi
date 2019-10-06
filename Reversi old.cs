79333b759213158308c101ce74204330aa9f7c7b
lemo_pen <lemopen55@gmail.com>
2019-10-06 12:12:52 +0900

    コンソールリバーシを作っていく

---
+++ Reversi old.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practice
+{ 
+
+    class MainClass
+    {
+        public static void drawChessboard()
+        {
+            int[,] board = new int[10, 17];
+            int Width = 17;
+            int Height = 10;
+            board[4, 4] = 1;
+            board[4, 5] = -1;
+            board[5, 4] = -1;
+            board[5, 4] = 1;
+
+            for (int y = 0; i < Height; i++)
+            {    
+                    Console.Write("+---------------+");         
+                    for (int j = 0; j < Width; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write("|");
+                        }
+                        else if (board[i, j] == 0)
+                        {
+                            Console.Write(".");
+                        }
+                        else if (board[i, j] == 1)
+                        {
+                            Console.Write("●");
+                        }
+                        else
+                        {
+                            Console.Write("◯");
+                        }
+                    }            
+                Console.WriteLine();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Game start");
+
+            //０１２３４５６７８ Width9 Height9
+            //２・・・・・・・・
+            //３・・・・・・・・
+            //４・・・〇仝・・・
+            //５・・・仝〇・・・
+            //６・・・・・・・・
+            //７・・・・・・・・
+            //８・・・・・・・・
+
+            //+---------------+ Width17 Height10
+            //|.|.|.|.|.|.|.|.|
+            //|.|.|.|.|.|.|.|.|
+            //|.|.|.|.|.|.|.|.|
+            //|.|.|.|●|◯|.|.|.|
+            //|.|.|.|◯|●|.|.|.|
+            //|.|.|.|.|.|.|.|.|
+            //|.|.|.|.|.|.|.|.|
+            //|.|.|.|.|.|.|.|.|
+            //+---------------+
+
+            //初期盤面の描写
+            drawChessboard();
+            
+            //ゲーム開始
+            Console.WriteLine("Enter your placement");
+        }
+    }
+}
---
+++ Reversi.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practice
+{
+    class Program
+    {
+        private static bool UTF8Encoding(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DrawGameboard(int[,]board)
+        {
+            string stringUTF8 = someFunctionReturnsUtf8();
+            byte[] bytesUTF8 = System.Text.Encoding.Default.GetBytes(stringUTF8);
+            string stringSJIS = System.Text.Encoding.UTF8.GetString(bytesUTF8);
+
+            Console.Clear();
+            Console.WriteLine("ＡＢＣＤＥＦＧＨ");
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    //if (x == 0 + y)
+                    //{
+                    //    Console.Write();
+                    //}
+                    if (board[x, y] == 0)
+                    {
+                        Console.Write("・");
+                    }
+                    else if (board[x, y] == 1)
+                    {
+                        Console.Write("●");
+                    }
+                    else
+                    {
+                        Console.Write("◯");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string someFunctionReturnsUtf8()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Reversi(string[] args)
+        {
+            Console.WriteLine("Game start");
+            //　１２３４５６７８
+            //Ａ・・・・・・・・
+            //Ｂ・・・・・・・・
+            //Ｃ・・・・・・・・
+            //Ｄ・・・❍●・・・
+            //Ｅ・・・●❍・・・
+            //Ｆ・・・・・・・・
+            //Ｇ・・・・・・・・
+            //Ｈ・・・・・・・・
+
+            //初期盤面の描写
+            int[,] board = new int[9, 9];
+            board[4, 4] = 1;
+            board[4, 5] = -1;
+            board[5, 4] = -1;
+            board[5, 4] = 1;
+            int width = board.GetLength(0);
+            int height = board.GetLength(1);
+            DrawGameboard(board);
+
+            //ゲーム開始
+            Console.WriteLine("Enter your placement");
+        }
+    }
+}
+
+//stringUTF8に何らかUTF8の文字列が入ってくる
+//string stringUTF8 = someFunctionReturnsUtf8();
+
+//まずはバイト配列に変換する
+//byte[] bytesUTF8 = System.Text.Encoding.Default.GetBytes(stringUTF8);
+
+//バイト配列をUTF8の文字コードとしてStringに変換する
+//string stringSJIS = System.Text.Encoding.UTF8.GetString(bytesUTF8);
